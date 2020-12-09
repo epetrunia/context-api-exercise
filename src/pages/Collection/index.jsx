b17863972dfff31df14c588e988af286cf542be7
Eugenia Petrunia <avepetrunia@gmail.com>
2020-12-09 14:27:51 +0200

    Refactor code with useContext hook

--- src/pages/Collection/index.jsx
+++ src/pages/Collection/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import CollectionItem from '../../components/CollectionItem';
 
@@ -7,27 +7,21 @@ import {
   CollectionTitle,
   CollectionItems,
 } from './Collection.style';
-
 import CollectionContext from './../../contexts/collections/collections.context';
 
 function Collection({ match }) {
+  const collections = useContext(CollectionContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
   return (
-    <CollectionContext.Consumer>
-      {(collections) => {
-        const collection = collections[match.params.collectionId];
-        const { title, items } = collection;
-        return (
-          <CollectionContainer>
-            <CollectionTitle>{title}</CollectionTitle>
-            <CollectionItems>
-              {items.map((item) => (
-                <CollectionItem key={item.id} item={item} />
-              ))}
-            </CollectionItems>
-          </CollectionContainer>
-        );
-      }}
-    </CollectionContext.Consumer>
+    <CollectionContainer>
+      <CollectionTitle>{title}</CollectionTitle>
+      <CollectionItems>
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </CollectionItems>
+    </CollectionContainer>
   );
 }
 
