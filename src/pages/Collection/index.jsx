23e1f7d98396d147c2b93903bb525986a6bad4f1
Eugenia Petrunia <avepetrunia@gmail.com>
2020-12-09 14:21:06 +0200

    Add context to Collection page

--- src/pages/Collection/index.jsx
+++ src/pages/Collection/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/CollectionItem';
 
@@ -8,24 +7,28 @@ import {
   CollectionTitle,
   CollectionItems,
 } from './Collection.style';
-import { selectCollection } from './../../redux/shop/selectors';
 
-function Collection({ collection }) {
-  const { title, items } = collection;
+import CollectionContext from './../../contexts/collections/collections.context';
+
+function Collection({ match }) {
   return (
-    <CollectionContainer>
-      <CollectionTitle>{title}</CollectionTitle>
-      <CollectionItems>
-        {items.map((item) => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </CollectionItems>
-    </CollectionContainer>
+    <CollectionContext.Consumer>
+      {(collections) => {
+        const collection = collections[match.params.collectionId];
+        const { title, items } = collection;
+        return (
+          <CollectionContainer>
+            <CollectionTitle>{title}</CollectionTitle>
+            <CollectionItems>
+              {items.map((item) => (
+                <CollectionItem key={item.id} item={item} />
+              ))}
+            </CollectionItems>
+          </CollectionContainer>
+        );
+      }}
+    </CollectionContext.Consumer>
   );
 }
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state),
-});
-
-export default connect(mapStateToProps)(Collection);
+export default Collection;
